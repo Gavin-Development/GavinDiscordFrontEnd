48ebcc84865fba2b97096054d4c2f1b30408f915
Scot_Survivor <unconfigured@null.spigotmc.org>
2021-11-23 22:46:33 +0000

    Remove SQL injection vulnerabilities.

--- DatabaseTools/tool.py
+++ DatabaseTools/tool.py
@@ -49,8 +49,8 @@ def sql_insert_into_message_logs(message_content: str, attachment_links: str, le
     else:
         insult = 0
     try:
-        sql = f"""INSERT INTO message_logs (message_content, attachment_links, length, positive, negative, neutral, compound, insult, unix) VALUES ('{message_content}', '{attachment_links}', {length}, {pos}, {neg}, {neu}, {compound}, {insult}, {int(dt.datetime.utcnow().timestamp())})"""
-        cursor.execute(sql)
+        sql = """INSERT INTO message_logs (message_content, attachment_links, length, positive, negative, neutral, compound, insult, unix) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);"""
+        cursor.execute(sql, (message_content, attachment_links, length, pos, neg, neu, compound, insult, dt.datetime.now().timestamp()))
         connection.commit()
     except Exception as e:
         raise e
@@ -61,9 +61,9 @@ def sql_insert_into_message_logs(message_content: str, attachment_links: str, le
 
 async def sql_insert_into_chat_logs(guild: int, channel: str, model: str, author: str, message: str, reply: str, date: int, connection: sqlite3.Connection, cursor: sqlite3.Cursor):
     if None not in (connection, cursor):
-        sql = f"""INSERT INTO chat_logs (guild, channel, model, author, ctx, reply, date) VALUES ({guild}, '{channel}', '{model}', '{author}', '{message}', '{reply}', {date})"""
+        sql = """INSERT INTO chat_logs (guild, channel, model, author, ctx, reply, date) VALUES (?, ?, ?, ?, ?, ?, ?);"""
         try:
-            cursor.execute(sql)
+            cursor.execute(sql, (guild, channel, model, author, message, reply, date))
             connection.commit()
         except Exception as e:
             return f"Insertion Error: {e}"
@@ -75,9 +75,9 @@ async def sql_insert_into_chat_logs(guild: int, channel: str, model: str, author
 
 def sql_retrieve_last_10_messages(guild: int, connection: sqlite3.Connection, cursor: sqlite3.Cursor):
     if None not in (connection, cursor):
-        sql = f"""SELECT * FROM chat_logs WHERE guild == {guild} ORDER BY date LIMIT 10;"""
+        sql = """SELECT * FROM chat_logs WHERE guild == ? ORDER BY date DESC LIMIT 10;"""
         try:
-            cursor.execute(sql)
+            cursor.execute(sql, (guild,))
             results = cursor.fetchall()
         except Exception as e:
             return f"Retrieval Error: {e}."
@@ -86,8 +86,8 @@ def sql_retrieve_last_10_messages(guild: int, connection: sqlite3.Connection, cu
 
 
 def sql_update_setting(guild: int, setting: str, value: str, cursor: sqlite3.Cursor, connection: sqlite3.Connection) -> bool:
-    sql = """SELECT value from settings WHERE guildID == {} and setting = "{}";""".format(guild, setting)
-    cursor.execute(sql)
+    sql = """SELECT value from settings WHERE guildID == ? and setting = ?;"""
+    cursor.execute(sql, (guild, setting))
     results = cursor.fetchall()
     update = False
     for result in results:
@@ -98,18 +98,19 @@ def sql_update_setting(guild: int, setting: str, value: str, cursor: sqlite3.Cur
             continue
     else:
         if update:
-            sql = """UPDATE settings SET value = "{}" WHERE setting = "{}" AND guildID = {};""".format(value, setting, guild)
+            sql = """UPDATE settings SET value = ? WHERE setting = ? AND guildID = ?;"""
+            cursor.execute(sql, (value, setting, guild))
         else:
-            sql = """INSERT INTO settings (guildID, setting, value) VALUES ({}, "{}", "{}");""".format(guild, setting,
-                                                                                                       value)
-        cursor.execute(sql)
+            sql = """INSERT INTO settings (guildID, setting, value) VALUES (?, ?, ?);"""
+            cursor.execute(sql, (guild, setting, value))
+
         connection.commit()
         return True
 
 
 def sql_retrieve_setting(guild: int, setting: str, cursor: sqlite3.Cursor) -> str or None:
-    sql = """SELECT value from settings where guildID == {} AND setting == "{}";""".format(guild, setting)
-    cursor.execute(sql)
+    sql = """SELECT value from settings where guildID == ? AND setting == ?;"""
+    cursor.execute(sql, (guild, setting))
     results = cursor.fetchall()
     try:
         return results[0]
@@ -118,17 +119,17 @@ def sql_retrieve_setting(guild: int, setting: str, cursor: sqlite3.Cursor) -> st
 
 
 def returnDisabled(guild, cursor: sqlite3.Cursor) -> list:
-    sql = """SELECT moduleName, commandName, channelID FROM disabled_commands WHERE guildID == {}""".format(guild)
-    cursor.execute(sql)
+    sql = """SELECT moduleName, commandName, channelID FROM disabled_commands WHERE guildID == ?"""
+    cursor.execute(sql, (guild,))
     return cursor.fetchall()
 
 
 def sql_insert_command_disabled_commands(guild: int, channel: int, module: str, command: str, cursor: sqlite3.Cursor, d_connection: sqlite3.Connection) -> bool or str:
     check = sql_check_disabled_commands(guild, channel, command, module, cursor)
     if check:
-        sql = """INSERT INTO disabled_commands (guildID, channelID, moduleName, commandName) VALUES (?, ?, ?, ?)""".format(guild, channel, module, command)
+        sql = """INSERT INTO disabled_commands (guildID, channelID, moduleName, commandName) VALUES (?, ?, ?, ?);"""
         try:
-            cursor.execute(sql)
+            cursor.execute(sql, (guild, channel, module, command))
             d_connection.commit()
         except Exception as e:
             return f"Insertion error: {e}"
@@ -139,8 +140,8 @@ def sql_insert_command_disabled_commands(guild: int, channel: int, module: str, 
 
 
 def sql_check_disabled_commands(guild: int, channel: int, command: str, module: str, cursor: sqlite3.Cursor) -> bool:
-    sql = """SELECT commandName FROM disabled_commands WHERE guildID == ? AND channelID == ? AND moduleName == ?;""".format(guild, channel, module)
-    cursor.execute(sql)
+    sql = """SELECT commandName FROM disabled_commands WHERE guildID == ? AND channelID == ? AND moduleName == ?;"""
+    cursor.execute(sql, (guild, channel, module))
     results = cursor.fetchall()
     for returns in results:
         if command in returns:
